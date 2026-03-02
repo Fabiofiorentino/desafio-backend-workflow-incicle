e52a253d51f40455a29d6fd67e0b879f391fdb4c
Fabiofiorentino <fabiovilaas@gmail.com>
2026-03-02 15:10:19 -0300

    REFACT: retira configuração swagger e adiciona ValidationPipes

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { TenantInterceptor } from './common/tenant.interceptor';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
-  const config = new DocumentBuilder()
-    .setTitle('Workflow InCicle API')
-    .setDescription('Desafio Backend Workflow InCicle')
-    .setVersion('1.0')
-    .build();
-
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('docs', app, document);
-
-  await app.listen(process.env.APP_PORT || 3000);
   app.useGlobalInterceptors(new TenantInterceptor());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+  await app.listen(process.env.APP_PORT || 3000);
 }
 bootstrap();
