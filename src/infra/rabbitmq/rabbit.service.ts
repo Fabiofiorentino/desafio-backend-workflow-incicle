00bfd7a5b5c1ea9cad786f9514e357eb1d5fca2d
Fabiofiorentino <fabiovilaas@gmail.com>
2026-03-03 07:58:13 -0300

    UPDATE: Atualiza rabbit.service

--- src/infra/rabbitmq/rabbit.service.ts
+++ src/infra/rabbitmq/rabbit.service.ts
@@ -1,17 +1,38 @@
 import * as amqp from 'amqplib';
-import { Injectable, OnModuleInit } from '@nestjs/common';
+import { Injectable, OnModuleInit, Logger } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class RabbitService implements OnModuleInit {
   private connection: amqp.ChannelModel;
   private channel: amqp.Channel;
+  private readonly logger = new Logger(RabbitService.name);
 
   constructor(private config: ConfigService) {}
 
   async onModuleInit() {
-    this.connection = await amqp.connect(this.config.get('ASYNC_URL'));
-    this.channel = await this.connection.createChannel();
+    await this.connectWithRetry();
+  }
+
+  private async connectWithRetry(retries = 10, delay = 3000) {
+    const url = this.config.get<string>('ASYNC_URL');
+
+    for (let i = 0; i < retries; i++) {
+      try {
+        this.connection = await amqp.connect(url);
+        this.channel = await this.connection.createChannel();
+        this.logger.log('RabbitMQ conectado');
+        return;
+      } catch (err) {
+        this.logger.warn(
+          `RabbitMQ indisponível. Tentativa ${i + 1}/${retries}`,
+        );
+        console.error(err);
+        await new Promise((res) => setTimeout(res, delay));
+      }
+    }
+
+    throw new Error('Não foi possível conectar ao RabbitMQ');
   }
 
   async publish(queue: string, message: any) {
